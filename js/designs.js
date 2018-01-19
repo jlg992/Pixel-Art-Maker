a84df3ea08f3db3817f5c8c2687e7cacee66e81f
Jeffery Graham <jeffery.lemar.graham@gmail.com>
2018-01-18 21:17:59 -0500

    Add function to make grid

--- js/designs.js
+++ js/designs.js
@@ -1,10 +1,29 @@
+const submitButton = document.querySelector("input[type='submit']");
+const pixelCanvas = document.getElementById('pixel_canvas');
+const colorPicker = document.getElementById("colorPicker");
+const heightInput = document.getElementById('input_height');
+const widthInput = document.getElementById('input_width');
+
 // Select color input
+let colorSelected = colorPicker.value
+
 // Select size input
+let heightChosen = heightInput.value;
+let widthChosen = widthInput.value;
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
-// Your code goes here!
-
+  for (let i = 0; i < heightChosen; i++) {
+    // creates a table row
+    let row = document.createElement("tr");
+    for (let j = 0; j < widthChosen; j++) {
+      // Create a <td> element and put the <td> at
+      // the end of the table row
+      let cell = document.createElement("td");
+      row.appendChild(cell);
+    }
+    // add the row to the end of the table body
+    pixelCanvas.appendChild(row);
+  }
 }
